980867fdf16d0cb9964ed53532b02ea3c9a68fa6
jeff-K24 <jeff.iezzi@kfzteile24.de>
2021-04-22 11:27:37 +0200

    Adding queries

---
+++ pdm_queries/ad-hoc/pdm-36_counts.sql
@@ -0,0 +1,44 @@
+SELECT
+  T1.[IsActive], 
+  T1.[IsBuyableInWebshop <DE>], 
+
+  base.[ArticleID], 
+  base.[K24Number]
+
+/*
+  T2.[ArticleStatus:Link], 
+  
+  base.[Manufacturer:Link], 
+  base.[ManufacturerArticleNo], 
+  
+  T3.[ArticleDesignation <de>], 
+
+  -- Categories
+  T4.[GenericArticleNo], 
+  T4.[Designation <de>], 
+
+  -- Catman
+  T6.[Name], 
+  
+  -- BI data
+  T7.[ABCClassificationK24], 
+  T7.[TurnoverRankK24], 
+  T7.[TotalStockQuantity]
+*/
+
+ FROM [dbo].[Article.Articles] base WITH (NOLOCK) 
+  LEFT OUTER JOIN [dbo].[Article.Articles:Catalog] T1 WITH (NOLOCK) ON T1.[:Id] = [base].[:Id]
+/* 
+  LEFT OUTER JOIN [dbo].[Article.Articles:ArticleProperties] T2 WITH (NOLOCK) ON T2.[:Id] = [base].[:Id]
+  LEFT OUTER JOIN [dbo].[Article.Articles:Texts] T3 WITH (NOLOCK) ON T3.[:Id] = [base].[:Id]
+  LEFT OUTER JOIN [dbo].[MasterData.GenericArticles] T4 WITH (NOLOCK) ON T4.[GenericArticleNo] = [T2].[GenericArticle:Link]
+  LEFT OUTER JOIN [dbo].[MasterData.SubCategories] T5 WITH (NOLOCK) ON T5.[SubCategoryNo] = [T2].[SubCategory:Link]
+  LEFT OUTER JOIN [dbo].[MasterData.ResponsiblePersonsForArticles] T6 WITH (NOLOCK) ON T6.[ResponsiblePersonNo] = [T5].[ResponsiblePerson:Link]
+  LEFT OUTER JOIN [dbo].[Article.Articles:DataWarehouseInformation] T7 WITH (NOLOCK) ON T7.[:Id] = [base].[:Id]
+*/
+ 
+ WHERE
+  T1.[IsActive] IS NULL 
+
+ORDER BY
+  T1.[IsBuyableInWebshop <DE>] 
