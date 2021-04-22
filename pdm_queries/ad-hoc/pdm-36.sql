6de79787818ee263f7cb7a3be06390a7a528fa05
jeff-K24 <jeff.iezzi@kfzteile24.de>
2021-04-22 11:27:55 +0200

    Making a real query.

--- pdm_queries/ad-hoc/pdm-36.sql
+++ pdm_queries/ad-hoc/pdm-36.sql
@@ -1,24 +1,41 @@
-SELECT
-  ArticleID,
-  K24Number,
-  Manufacturer,
-  ManufacturerArticleNo,
-  Texts:ArticleDesignation[de],
-  ArticleProperties:GenericArticle,
-  ArticleProperties:SubCategory>ResponsiblePerson>Name,
-  Catalog:IsBuyableInWebshop[DE],
-  DataWarehouseInformation:ABCClassificationK24,
-  ArticleProperties:ArticleStatus,
-  DataWarehouseInformation:SalesQuantity12MonthsRolling,
-  DataWarehouseInformation:TotalStockQuantity,
-  ArticleProperties:IsBulkyGoods,
-  Catalog:IsActive
+SELECT TOP 10
+  T1.[IsActive], 
+  T1.[IsBuyableInWebshop <DE>], 
+  T2.[ArticleStatus:Link], 
+  
+  base.[ArticleID], 
+  base.[K24Number], 
+  base.[Manufacturer:Link], 
+  base.[ManufacturerArticleNo], 
+  
+  T3.[ArticleDesignation <de>], 
 
-FROM Article.Articles
+  -- Categories
+  T4.[GenericArticleNo], 
+  T4.[Designation <de>], 
 
-WHERE (PLACEHOLDER)
-  AND (K24Number IS NOT NULL)
-  AND (ArticlesHasManufacturerPrioForCountryFilterProvider('DE'))
-  AND (Catalog:IsBuyableInWebshop[DE] = true)
+  -- Catman
+  T6.[Name], 
+  
+  -- BI data
+  T7.[ABCClassificationK24], 
+  T7.[TurnoverRankK24], 
+  T7.[TotalStockQuantity]
+ 
+ FROM [dbo].[Article.Articles] base WITH (NOLOCK) 
+  LEFT OUTER JOIN [dbo].[Article.Articles:Catalog] T1 WITH (NOLOCK) ON T1.[:Id] = [base].[:Id]
+  LEFT OUTER JOIN [dbo].[Article.Articles:ArticleProperties] T2 WITH (NOLOCK) ON T2.[:Id] = [base].[:Id]
+  LEFT OUTER JOIN [dbo].[Article.Articles:Texts] T3 WITH (NOLOCK) ON T3.[:Id] = [base].[:Id]
+  LEFT OUTER JOIN [dbo].[MasterData.GenericArticles] T4 WITH (NOLOCK) ON T4.[GenericArticleNo] = [T2].[GenericArticle:Link]
+  LEFT OUTER JOIN [dbo].[MasterData.SubCategories] T5 WITH (NOLOCK) ON T5.[SubCategoryNo] = [T2].[SubCategory:Link]
+  LEFT OUTER JOIN [dbo].[MasterData.ResponsiblePersonsForArticles] T6 WITH (NOLOCK) ON T6.[ResponsiblePersonNo] = [T5].[ResponsiblePerson:Link]
+  LEFT OUTER JOIN [dbo].[Article.Articles:DataWarehouseInformation] T7 WITH (NOLOCK) ON T7.[:Id] = [base].[:Id]
+ 
+ WHERE
+  T1.[IsActive] IS NULL 
 
-ORDER BY DataWarehouseInformation:SalesQuantity12MonthsRolling DESC;
+ ORDER BY
+   T6.[Name],
+   T7.[ABCClassificationK24],
+   base.[Manufacturer:Link],
+   base.[ArticleID]
