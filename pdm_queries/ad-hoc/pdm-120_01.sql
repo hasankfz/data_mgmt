e990dcba39f9d4e9bc24e2967b7cfd093028ed45
jeff-K24 <jeff.iezzi@kfzteile24.de>
2021-04-23 09:15:03 +0200

    Minor tweeks

--- pdm_queries/ad-hoc/pdm-120_01.sql
+++ pdm_queries/ad-hoc/pdm-120_01.sql
@@ -11,13 +11,11 @@ SELECT TOP 100
   T2.[TecDoc.ArtNo],
 
   base.[ManufacturerArticleNo]
-  
-  
+    
 FROM [dbo].[Article.Articles] base WITH (NOLOCK) 
   LEFT OUTER JOIN [dbo].[Article.Articles:ArticleProperties] T1 WITH (NOLOCK) ON T1.[:Id] = [base].[:Id]
   LEFT OUTER JOIN [dbo].[Article.Articles:TecDocData] T2 WITH (NOLOCK) ON T2.[:Id] = [base].[:Id]
   LEFT OUTER JOIN [dbo].[TecDoc.Articles.Articles] T3 WITH (NOLOCK) ON T3.[ArticleNo] = T2.[TecDoc.ArtNo]
- 
  
 WHERE base.[K24Number] IS NOT NULL
   AND T2.[TecDoc.ArtNo] IS NOT NULL
--- pdm_queries/ad-hoc/pdm-120_02.sql
+++ pdm_queries/ad-hoc/pdm-120_02.sql
@@ -3,22 +3,22 @@
 */
 
 SELECT TOP 100 
-
-  T3.[Name] as "Motor", 
-  T1.[PassengerCarNo], 
-  T5.[Name], 
-  T6.[Designation], 
+  T3.[Name] as "EngineType", 
+  T1.[PassengerCarNo] as "CarNr", 
+  T5.[Name] as "CarMake", 
+  T6.[Designation] as "CarModel", 
   T10.[Designation] as "K24-Kategorie", 
   T8.[Designation] as "TD-Genart", 
-  T11.[ArticleNo] as "TD-ArtNr.", 
---  T12.[Designation] as "TD-ArtBeschreibung"
+  T11.[ArticleNo] as "TD-ArtNr", 
+  td_art.[TecDoc.ArtNo] as "PDM-TD-ArtNr",
 
-  td_art.[TecDoc.ArtNo] as "Art-TDArtNr",
-  td_art.[:Id],
-  art.[:Id],
+  T12.[Designation] as "TD-ArtBeschreibung",
 
-  art.[ArticleID], 
-  art.[K24Number] 
+  art.[ArticleID] as "PDM-ArtID", 
+  art.[K24Number] as "K24-Nr",
+
+  td_art.[:Id] as "TD-ArtID",
+  art.[:Id] as "PDM-ArtID"
 
  FROM [dbo].[TecDoc.Linkages.PassengerCars] base WITH (NOLOCK) 
   LEFT OUTER JOIN [dbo].[TecDoc.LinkingTargets.PassengerCars] T1 WITH (NOLOCK) ON T1.[PassengerCarNo] = [base].[LinkingTarget:Link]
@@ -42,4 +42,16 @@ SELECT TOP 100
   LEFT OUTER JOIN [dbo].[Article.Articles:TecDocData] td_art WITH (NOLOCK) ON td_art.[TecDoc.ArtNo] = T11.[ArticleNo]
   LEFT OUTER JOIN [dbo].[Article.Articles] art WITH (NOLOCK) ON art.[:Id] = td_art.[:Id]
 
-WHERE art.[K24Number] IS NOT NULL
+WHERE
+  art.[K24Number] IS NOT NULL
+  /*
+  AND 
+  (
+  -- EngineType
+    ((T3.[Name] = 'Elektromotor') 
+      OR (T3.[Name] = 'Mild Hybrid')
+	  OR (T3.[Name] = 'Full Hybrid')
+	  OR (T3.[Name] = 'Plug-In Hybrid'))
+  AND (T11.[State:Link] = '73-001') -- TecDoc Status
+  )
+*/
