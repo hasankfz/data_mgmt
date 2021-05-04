d581aff12460facf87f61451e6028dfb776eac89
jeff-K24 <jeff.iezzi@kfzteile24.de>
2021-05-04 15:25:07 +0200

    Updated query to match PDM view

--- pdm_queries/weekly/DE_neuartikel.sql
+++ pdm_queries/weekly/DE_neuartikel.sql
@@ -3,17 +3,17 @@
   Description: Send the results of a weekly query to the retail shops that summarizes the new products in our sortiment. 
 */
 
-SELECT TOP 100
-  base.[ArticleID], 
-  base.[Manufacturer:Link], 
-  base.[K24Number], 
-  base.[ManufacturerArticleNo], 
-  base.[InternalEAN], 
-  T1.[ArticleStatus:Link], 
+SELECT --TOP 100
+  base.[ArticleID] as "Artikel-ID", 
+  base.[K24Number] as "K24-Nr", 
+  base.[Manufacturer:Link] as "Hersteller", 
+  base.[ManufacturerArticleNo] as "Hersteller-Art-Nr", 
+  base.[InternalEAN] as "EAN", 
+  T1.[ArticleStatus:Link] as "Status", 
   (CASE WHEN T2.[IsActiveSince] IS NULL THEN NULL
      ELSE CAST(YEAR(T2.[IsActiveSince]) as varchar(4)) + '-' + RIGHT('00' + CAST(MONTH(T2.[IsActiveSince]) as varchar(2)), 2) + '-' + RIGHT('00' + CAST(DAY(T2.[IsActiveSince]) as varchar(2)), 2)
-   END), 
-  T3.[SalesQuantity12MonthsRolling]
+   END) as "Aktiv Seit", 
+  T3.[SalesQuantity12MonthsRolling] as "Verkaufsquantität"
  
 FROM [dbo].[Article.Articles] base WITH (NOLOCK) 
   LEFT OUTER JOIN [dbo].[Article.Articles:ArticleProperties] T1 WITH (NOLOCK) ON T1.[:Id] = [base].[:Id]
@@ -22,7 +22,7 @@ FROM [dbo].[Article.Articles] base WITH (NOLOCK)
  
 WHERE
  -- (((T2.[IsActiveSince] >= @p114) AND (T2.[IsActiveSince] < @p115)))
-  CURRENT_TIMESTAMP >= DATEADD(week, DATEDIFF(week,0,GETDATE())-1,-1)
+  T2.[IsActiveSince] >=  DATEADD(wk, DATEDIFF(wk,7,GETDATE()), 0)
 
 ORDER BY
    T3.[SalesQuantity12MonthsRolling] DESC,
