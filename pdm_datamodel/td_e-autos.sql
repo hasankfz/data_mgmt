6dcbc162a76c8f48b276117b34cb7c539e705065
jeff-K24 <jeff.iezzi@kfzteile24.de>
2021-05-09 13:30:52 +0200

    Refactoring and cleaning

--- pdm_datamodel/td_e-autos.sql
+++ pdm_datamodel/td_e-autos.sql
@@ -1,21 +1,21 @@
 /*
-   List the electric vehicles in TecDoc 
+   List the electric vehicles in TecDoc that are available in the German market
 */
 
 SELECT DISTINCT
   --  COUNT(pc.[:Id]) 926
-  T1.LongDesignation,
-  pc.PassengerCarNo
+  td_pc_de.LongDesignation,
+  td_pc.PassengerCarNo
 
-FROM dbo.[TecDoc.LinkingTargets.PassengerCars] pc WITH (NOLOCK) 
-  LEFT OUTER JOIN [dbo].[TecDoc.LinkingTargets.PassengerCars <TecDoc.GeneralData.UsedCountries>] T1 WITH (NOLOCK) ON T1.[:Id] = pc.[:Id]
-              AND T1.[:TecDoc.GeneralData.UsedCountries_Id] = 'fa28054b-7d56-4c8a-a303-cbaa1df0e43d'
-  LEFT OUTER JOIN dbo.[TecDoc.GeneralData.KeyValues] T4 WITH (NOLOCK) ON T4.[KeyValueNo] = pc.[EngineType:Link]
+FROM dbo.[TecDoc.LinkingTargets.PassengerCars] td_pc WITH (NOLOCK)
+  -- Get cars in the German market 
+  LEFT OUTER JOIN [dbo].[TecDoc.LinkingTargets.PassengerCars <TecDoc.GeneralData.UsedCountries>] td_pc_de WITH (NOLOCK) ON td_pc_de.[:Id] = td_pc.[:Id]
+              AND td_pc_de.[:TecDoc.GeneralData.UsedCountries_Id] = 'fa28054b-7d56-4c8a-a303-cbaa1df0e43d'
 
 WHERE
-  pc.[EngineType:Link] IN ('80-040','80-046','80-048')
+  td_pc.[EngineType:Link] IN ('80-040','80-046','80-048')
   AND
-  pc.ImportVersionNo = '20210401'
+  td_pc.ImportVersionNo = '20210401'
 
 ORDER BY
-  T1.LongDesignation
+  td_pc_de.LongDesignation
