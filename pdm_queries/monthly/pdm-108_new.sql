f7910849d9542629244fef04abe47607d29d196e
danielnue <82215110+danielnue@users.noreply.github.com>
2021-06-08 13:29:15 +0200

    Update pdm-108_new.sql
    Added Verantwortlicher to the SQL-Statement.

--- pdm_queries/monthly/pdm-108_new.sql
+++ pdm_queries/monthly/pdm-108_new.sql
@@ -7,6 +7,7 @@ art.[Manufacturer:Link] AS HLK,
 art.ManufacturerArticleNo AS ArtN,
 art.K24Number,
 artText.[ArticleDesignation <de>] AS ArtikelText,
+mdResPers.[Name] AS Verantwortlicher,
 CAST(art.[:Log.CreatedAt] as DATE) AS created_at,
 CAST(art.[:Log.LastModAt] as DATE) AS lastmod_at
 
@@ -14,10 +15,10 @@ FROM [Article.Articles] AS art
      LEFT JOIN [Article.Articles:ArticleProperties] AS artProp ON art.[:Id] = artProp.[:Id]
      LEFT JOIN [Article.Articles:Texts] AS artText ON art.[:Id] = artText.[:Id]
      LEFT JOIN [Article.Articles:UDF ArticleManagement] AS artUDF ON art.[:Id] = artUDF.[:Id]
-WHERE
- artProp.[ArticleStatus:Link] = 1 
- AND
- artUDF.[Artikel ohne Bild] = 1
+     LEFT JOIN [MasterData.SubCategories] AS mdSubCat ON artProp.[SubCategory:Link] = mdSubCat.[:Caption]
+	LEFT JOIN [MasterData.ResponsiblePersonsForArticles] AS mdResPers ON mdSubCat.[ResponsiblePerson:Link] = mdResPers.[ResponsiblePersonNo]
+     
+WHERE artProp.[ArticleStatus:Link] = 1
+AND artUDF.[Artikel ohne Bild] = 1
 
- ORDER BY
- art.[:Log.LastModAt] DESC
+ORDER BY art.[:Log.LastModAt] DESC;
