11d0d11247ce04fab82a089973fddf880d9f8a91
jeff-K24 <jeff.iezzi@kfzteile24.de>
2021-06-16 11:46:38 +0200

    Refactored with help from CZ

--- pdm_queries/ad-hoc/pdm-158.sql
+++ pdm_queries/ad-hoc/pdm-158.sql
@@ -5,11 +5,12 @@ SELECT DISTINCT
     pdm_art.[ArticleID], 
     pdm_art.[K24Number] 
 --    pdm_art.[ManufacturerArticleNoWithoutFormat], 
+--   pdm_art.ManufacturerArticleNoWithoutSpaces
 --    art_td.[TecDoc.ArtNo]
 
-FROM [ji_reporting].[dbo].[pdm-158] ji 
+FROM [ji_reporting].[dbo].[pdm-158_import] ji 
    LEFT OUTER JOIN [dbo].[Article.Articles] pdm_art ON TRIM(ji.[HER-NR]) = pdm_art.[ManufacturerArticleNo]
-                                                    OR TRIM(ji.[HER-NR]) = pdm_art.[ManufacturerArticleNoWithoutFormat]
+                                                    OR TRIM(ji.[HER-NR]) = pdm_art.[ManufacturerArticleNoWithoutSpaces]
 -- TecDoc articles in the PDM
    LEFT OUTER JOIN [dbo].[Article.Articles:TecDocData] art_td ON pdm_art.[:Id] = art_td.[:Id] 
 
